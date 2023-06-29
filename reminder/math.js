7e68f3e20a30721b6133fec157efe5e9ebc752a0
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-29 18:14:29 +0300

    Initial commit

---
+++ reminder/math.js
@@ -0,0 +1,10 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+// export {sum};
+// export function sum () {}
+module.exports.sum = sum;
+console.log('math')
+// export default sum;
+// module.exports = sum;
