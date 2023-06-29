a9a90a9548f91a6d6e5f6e4e2861127f70a4645f
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-29 20:33:32 +0300

    Add basic middlewares example

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  // console.log(request);
-  // console.log(response);
-
-  // res.end('<h1>hello from express</h1>');
-  res.send('<h1>hello from express</h1>');
-});
+app.get(
+  '/',
+  (req, res, next) => {
+    console.log('first handler');
+    req.secret = '12345';
+    next();
+  },
+  (req, res, next) => {
+    console.log('second handler');
+    console.log(req.secret);
+    next();
+  },
+  (req, res, next) => {
+    // console.log(request);
+    // console.log(response);
+    console.log('last handler');
+    // res.end('<h1>hello from express</h1>');
+    res.send('<h1>hello from express</h1>');
+  }
+);
 // app.post();
 // app.put();
 // app.patch();
 // app.delete();
+
+app.get('/users');
 
 app.get('*', (req, res) => {
   res.send(`path is ${req.path} and method is ${req.method}`);
